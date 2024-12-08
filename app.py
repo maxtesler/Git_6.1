44db195dd50f2ddfb0b8df6c42c92e0dc94a88cb
Max Tesler <maxtesler@MacBook-Pro-Max.local>
2024-12-08 23:05:34 +0200

    Merge branch 'feat-mul'

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ first_num = float(input('Enter your first number: '))
 
 second_num = float(input('Enter your second number: '))
 
-fun = input('Choose +|- function: ')
+fun = input('Choose +|-|* function: ')
 
 def num_calc(first_num: float, second_num:float, fun:str):
    if fun == 'add' or fun == '+':
        result = first_num + second_num
    elif fun == 'subtract' or fun == '-':
        result = first_num - second_num
+   elif fun == '*':
+        result = first_num - second_num
    else: raise ValueError(f"Incorrect function: {fun} .")
   
    print(f'Your result is {result}')
