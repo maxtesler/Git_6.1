073ad129704c833c0dc05e8ddae9ce7ce8b895e5
Max Tesler <maxtesler@MacBook-Pro-Max.local>
2024-12-08 21:40:51 +0200

    Create calc func

---
+++ app.py
@@ -0,0 +1,19 @@
+first_num = float(input('Enter your first number: '))
+
+second_num = float(input('Enter your second number: '))
+
+fun = input('Choose +|- function: ')
+
+def num_calc(first_num: float, second_num:float, fun:str):
+   if fun == 'add' or fun == '+':
+       result = first_num + second_num
+   elif fun == 'subtract' or fun == '-':
+       result = first_num - second_num
+   else: raise ValueError(f"Incorrect function: {fun} .")
+  
+   print(f'Your result is {result}')
+
+try:
+   num_calc(first_num, second_num,fun)
+except ValueError as e:
+   print(e)
